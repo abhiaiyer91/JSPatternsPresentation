29f9ca8fd0d696671a5a1a7c85df4fd75583c555
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-02-11 08:53:26 -0800

    Space Dojo preso

--- presentation/index.js
+++ presentation/index.js
@@ -20,7 +20,7 @@ import {
   Slide,
   Spectacle,
   Text
-} from "spectacle";
+  } from "spectacle";
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
@@ -38,9 +38,10 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   blaze: require("../assets/blaze.jpg"),
-  matt: require("../assets/matt.jpg"),
   abhi: require("../assets/abhi.jpg"),
-  nardo: require("../assets/nardo.jpg"),
+  fatigue: require("../assets/fatigue.png"),
+  seissues: require("../assets/seissues.png"),
+  wp: require("../assets/wp.png"),
   redux: require("../assets/redux-2.png"),
   markdown: require("../assets/markdown.png")
 };
@@ -58,29 +59,37 @@ export default class Presentation extends React.Component {
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
           <Slide transition={["zoom"]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Welcome to Pop
+              When the world is in "Flux",
             </Heading>
             <Heading size={1} fit caps>
-              An opinionated way to build UI @ Workpop Inc.
+              you'll be fine.
             </Heading>
           </Slide>
           <Slide transition={["slide"]} bgColor="black">
-            <Image src={images.matt.replace("/", "")} margin="0px auto 40px" height="293px"/>
             <Image src={images.abhi.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Image src={images.nardo.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Heading size={2} caps fit textColor="white" textFont="primary">
+              <Link textColor="primary" href="https://www.twitter.com/abhiaiyer">@abhiaiyer</Link> Software Engineer,
+              <Link textColor="primary" href="https://www.workpop.com">Workpop Inc.</Link>
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Image src={images.wp.replace("/", "")} margin="0px auto 40px" height="293px"/>
             <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Brought to you by the Ninjaz
+              MeteorJS / ReactJS.
+            </Heading>
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
+              Native Android / iOS.
             </Heading>
           </Slide>
           <Slide transition={["slide"]} bgImage={images.blaze.replace("/", "")} bgDarken={0.75}>
             <Appear fid="1">
               <Heading size={2} caps fit textColor="primary">
-                The Past
+                The Problem
               </Heading>
             </Appear>
             <Appear fid="2">
               <Heading size={1} caps fit textColor="tertiary">
-                Blaze + Tracker
+                Software Engineering is always changing.
               </Heading>
             </Appear>
             <Appear fid="3">
@@ -90,95 +99,124 @@ export default class Presentation extends React.Component {
             </Appear>
           </Slide>
           <Slide transition={["slide"]} bgColor="black">
+            <Image src={images.seissues.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
+              The issues we face.
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Image src={images.fatigue.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
+              JavaScript Fatigue
+            </Heading>
+          </Slide>
+          <Slide transition={["zoom"]} bgColor="secondary">
+            <Heading size={2} fit caps lineHeight={1} textColor="primary">
+              So if things constantly change..
+            </Heading>
+            <Appear><Heading size={1} fit caps lineHeight={1} textColor="primary">
+              how do we manage complexity?
+            </Heading></Appear>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
             <BlockQuote>
               <Quote size={3}>Managing complexity is the most important technical topic in software development.</Quote>
               <Cite>Steve McConnell in Code Complete</Cite>
             </BlockQuote>
           </Slide>
           <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Lets Talk About State</Heading>
+            <Heading caps fit>Lets Talk About Architecture</Heading>
             <Layout>
               <Fill>
                 <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  UI State
+                  Frameworks
                 </Heading>
               </Fill>
               <Fill>
                 <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Domain State
+                  Business Logic
                 </Heading>
               </Fill>
             </Layout>
           </Slide>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Enter React
-            </Heading>
-          </Slide>
           <Slide transition={["zoom", "fade"]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Why React?
+              "Ah F.A.C.!!!!"
+            </Heading>
+            <Appear fid="1">
+              <Heading size={1} fit caps lineHeight={1} textColor="black">
+                Frameworks Always Change
+              </Heading>
+            </Appear>
+          </Slide>
+          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
+            <Heading caps fit size={1} textColor="primary">
+              Methods over results
             </Heading>
             <List>
-              <Appear><ListItem>Components</ListItem></Appear>
-              <Appear><ListItem>VirtualDOM</ListItem></Appear>
-              <Appear><ListItem>JSX</ListItem></Appear>
+              <ListItem>Write and maintain patterns</ListItem>
+              <ListItem>Decouple systems</ListItem>
+              <ListItem>Cogs in a machine</ListItem>
             </List>
           </Slide>
           <Slide transition={["spin", "zoom"]} bgColor="tertiary">
             <Heading caps fit size={1} textColor="primary">
-              Components
+              The Command Pattern
             </Heading>
             <List>
-              <ListItem>Breaking UI into a component hierarchy is logical</ListItem>
-              <ListItem>Components are highly reusable</ListItem>
-              <ListItem>JSX is the shit</ListItem>
-            </List>
-
-                  <aside class="notes">
-                    If you think in a React way, the concerns of the application will be small composable components.
-                  </aside>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              VirtualDOM
-            </Heading>
-            <List>
-              <ListItem>Efficiency</ListItem>
-              <ListItem>It has a full event system</ListItem>
-              <ListItem>No direct DOM manipulations</ListItem>
+              <ListItem>Separate the responsibilities </ListItem>
+              <ListItem>Delegation of actions</ListItem>
+              <ListItem>What Flux really is</ListItem>
             </List>
           </Slide>
           <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              This slideshow is one big React component
+            <Heading caps fit size={1} textColor="secondary">
+              Running a Grocery Store
             </Heading>
-            <Interactive/>
+            <CodePane
+              lang="js"
+              source={require("raw!../assets/deck.example")}
+              margin="20px auto"
+              />
           </Slide>
           <Slide transition={["slide", "spin"]} bgColor="primary">
             <Heading caps fit size={1} textColor="tertiary">
               But...
             </Heading>
             <Heading caps fit size={1} textColor="secondary">
-              What about Data Flow?
+              Why is this bad?
             </Heading>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <Heading caps fit size={1} textColor="primary">
+              If the core API changes we have to modify all the touchpoints.
+            </Heading>
+            <Heading caps fit size={1} textColor="primary">
+              Abstract the API away further.
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading caps fit size={1} textColor="secondary">
+              Command Pattern Revisited
+            </Heading>
+            <CodePane
+              lang="js"
+              source={require("raw!../assets/revisited.example")}
+              margin="20px auto"
+              />
           </Slide>
           <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
             <Heading caps fit size={1} textColor="primary">
               Enter Redux
             </Heading>
-            <List>
-              <ListItem>Manage state with a minimal but predictable API</ListItem>
-            </List>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <Image src={images.redux.replace("/", "")} margin="0px auto 40px" height="600px"/>
+            <Image src={images.redux.replace("/", "")} margin="0px auto 40px" height="500px"/>
           </Slide>
           <Slide transition={["spin", "slide"]} bgColor="tertiary">
             <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
               How can I get started?
             </Heading>
-            <Heading><Link textColor="secondary" href="https://workpop.gitbooks.io/pop/content/">Pop Book</Link></Heading>
+            <Heading><Link textColor="secondary" href="https://workpop.gitbooks.io/pop/content/">Pop
+              Book</Link></Heading>
           </Slide>
           <Slide transition={["spin", "slide"]} bgColor="tertiary">
             <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
